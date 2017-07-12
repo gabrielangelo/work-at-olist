850febd02020a49f33023e79de37ee96bd4207d3
gabrielangelo <=>
2017-07-12 17:55:09 -0300

    corrected save method when is created Channel

--- work-at-olist/workatolist/channels/models.py
+++ work-at-olist/workatolist/channels/models.py
@@ -7,6 +7,6 @@ from workatolist.categories.models import Category
 class Channel(models.Model):
     description = models.CharField(max_length=200)
 
-    def create(self):
+    def save(self, *args, **kwargs):
+        super(Channel, self).save(*args, **kwargs)
         Category.objects.create(title='category', channel=self)
-        return super(self.__class__, self).objects.create()

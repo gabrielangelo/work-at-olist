f5076982416ac92f783f520b0160da2e46d26a5b
gabrielangelo <=>
2017-07-24 16:59:18 -0300

    Refactoring of read's tree method, the code stay most clean and most readable. The addition of a parent foreign key make  all the difference !

--- src/categories/models.py
+++ src/categories/models.py
@@ -25,7 +25,6 @@ class Category(models.Model):
                 node_queryset.filter(rgt__gte=root.rgt, channel=root.channel).update(rgt=F('rgt') + 2)
                 node_queryset.filter(lft__gt=root.rgt, channel=root.channel).update(rgt=F('lft') + 2)
                 last_node = check_parents.filter(rgt=root.rgt-1).get()
-                #return cls(title=parent.title, channel=parent.channel, lft=last_node.rgt + 1, rgt=last_node.rgt + 2)
                 node_queryset.create(title=parent.title, channel=parent.channel, lft=last_node.rgt + 1,
                                      rgt=last_node.rgt + 2, parent=root)
             else:
@@ -33,7 +32,6 @@ class Category(models.Model):
                 node_queryset.filter(lft__gt=root.lft, channel=root.channel).update(lft=F('lft') + 2)
                 node_queryset.create(title=parent.title, channel=parent.channel, lft=root.lft + 1, rgt=root.lft + 2,
                                      parent=root)
-                #return cls(title=parent.title, channel=parent.channel, lft=root.lft + 1, rgt=root.lft + 2)
 
     @classmethod
     def make_json_tree(cls, parent):
@@ -47,56 +45,24 @@ class Category(models.Model):
         path = []
         t = tree()
         for node in nodes:
-            if node.parent_id is None:
+            if node.parent_id is None or node.parent_id == path[-1].id:
                 path.append(node)
                 dump_tree(t, path)
-            elif node.parent_id == path[-1:][-1].id:
+                # if node.lft == path[-1].rgt + 2:
+            elif node.lft == path[-1].rgt + 1:
+                path.pop()
                 path.append(node)
-                dump_tree(node)
-            elif node.lft == path[-1:][-1].rgt + 2:
-                
-        # path = []
-        # root = nodes.first()
-        # size = nodes.count()
-        # path.append(root)
-        # t = tree()
-        # dump_tree(t, path)
-
-        # for i in range(1, size - 1):
-        #     if nodes[i-1].lft == nodes[i].lft - 1:
-        #         path.append(nodes[i])
-        #         dump_tree(t, path)
-        #         # if i == size - 2:
-        #         #     path.append(nodes[i+1])
-        #         #     dump_tree(t, path)
-        #         if nodes[i+1].lft == nodes[i].rgt + 2:
-        #             path.pop()
-        #             aux = nodes[i]
-        #             nodes = list(nodes) if not isinstance(nodes, list) else nodes
-        #             nodes[i] = nodes[i-1]
-        #             nodes[i-1] = aux
-        #             # if i == size - 2:
-        #             #     path.append(nodes[i + 1])
-        #             #     dump_tree(t, path)
-        #     elif nodes[i-1].rgt == nodes[i].lft - 1 and nodes[i+1].lft == nodes[i].rgt + 2:
-        #         path.append(nodes[i])
-        #         dump_tree(t, path)
-        #         path.pop()
-        #         path.pop()
-        #         # if i == size - 2:
-        #         #     path.append(nodes[i+1])
-        #         #     dump_tree(t, path)
-        #     elif nodes[i-1].rgt == nodes[i].lft - 1:
-        #         path.pop()
-        #         path.append(nodes[i])
-        #         dump_tree(t, path)
-        #         path.pop()
-        #         # if i == size - 2:
-        #         #     path.append(nodes[i+1])
-        #         #     dump_tree(t, path)
-        #     elif nodes[i+1].lft == nodes[i].lft + 1:
-        #         path.append(nodes[i])
-        #         dump_tree(t, path)
+                dump_tree(t, path)
+            elif node.lft == path[-1].rgt + 2:
+                path.pop()
+                path.pop()
+                path.append(node)
+                dump_tree(t, path)
+            elif node.lft > path[-1].rgt +2:
+                while node.parent_id != path[-1].id:
+                    path.pop()
+                path.append(node)
+                dump_tree(t, path)
 
         return dicts(t)
 

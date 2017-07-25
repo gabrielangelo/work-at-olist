d87c31bce7b7bb0512b759a34d56ac23d2503b41
gabrielangelo <=>
2017-07-25 16:54:49 -0300

    Added title and channel filters in Category Viewset

--- src/categories/api/views.py
+++ src/categories/api/views.py
@@ -1,1 +1,41 @@
+from rest_framework.viewsets import ReadOnlyModelViewSet
+from rest_framework.permissions import AllowAny
+from categories.models import Category
+from categories.api.serializers import CategorySerializer
+from rest_framework.filters import DjangoFilterBackend
 
+
+class CategoriesViewset(ReadOnlyModelViewSet):
+    """filter categories by description's channel"""
+    lookup_field = 'channel__description'
+    permission_classes = [AllowAny, ]
+    serializer_class = CategorySerializer
+    filter_backends = (DjangoFilterBackend, )
+    filter_fields = ('title', 'channel__description')
+
+    class Meta:
+        model = Category
+
+    def get_queryset(self):
+        channel = self.request.query_params.get('channel', None)
+        title = self.request.query_params.get('title', None)
+        if title and channel:
+            return self.Meta.model.objects.prefetch_related('channel').filter(channel__description=channel, title=title)
+        elif channel:
+            return self.Meta.model.objects.prefetch_related('channel').filter(lft=1, channel__description=channel)
+        elif title:
+            return self.Meta.model.objects.prefetch_related('channel').filter(title=title)
+        else:
+            return self.Meta.model.objects.prefetch_related('channel').filter(lft=1)
+
+    def list(self, request, *args, **kwargs):
+        '''
+        filter categories by title and/or channel description. If 
+        a category is filtered only by title, categories with 
+        this title(if exists) can be sought on all channels. If the same 
+        is filtered only by channel, all categories from this channel(if exists), 
+        will be sought. If the two query parameters coexist, one category(if exists)
+        with her subcategories of a channel will be sought.
+        All data are organized by a json hierarchical structure 
+        '''
+        return super(self.__class__, self).list(request, *args, **kwargs)

fe8d3fea5989d98a548eafc13fb8526a6fb916ab
gabriel <gabriel@outkeyapp.com>
2017-07-26 09:29:31 -0300

    Refactoring of get_queryset method in CategoriesViewset

--- src/categories/api/views.py
+++ src/categories/api/views.py
@@ -1,4 +1,6 @@
 from django.http import Http404
+from django.shortcuts import get_object_or_404
+from rest_framework.generics import RetrieveAPIView
 from rest_framework.viewsets import ReadOnlyModelViewSet
 from rest_framework.permissions import AllowAny
 from categories.models import Category
@@ -13,41 +15,39 @@ class CategoriesViewset(ReadOnlyModelViewSet):
     serializer_class = CategorySerializer
     filter_backends = (DjangoFilterBackend, )
     filter_fields = ('title', 'channel__description')
+    queryset = Category.objects.filter(lft=1)
 
     class Meta:
         model = Category
 
     def get_queryset(self):
-        channel = self.request.query_params.get('channel__description', None)
-        title = self.request.query_params.get('title', None)
-        if title and channel:
-            queryset = self.Meta.model.objects.prefetch_related('channel').filter(channel__description=channel,
-                                                                                  title=title)
-            if not queryset.exists():
-                raise Http404
-            return queryset
+        filter = {}
+        query_params_size = len(self.request.query_params)
+        if query_params_size > 0:
+            for field in self.filter_fields:
+                param = self.request.query_params.get(field, None)
+                if param:
+                    filter[field] = param
+                else:
+                    pass
+            if 'channel__description' in filter.keys() and query_params_size == 1:
+                filter['lft'] = 1
+                self.Meta.model.objects.filter(**filter)
 
-        elif channel:
-            queryset = self.Meta.model.objects.prefetch_related('channel').filter(lft=1, channel__description=channel)
+            queryset = self.Meta.model.objects.filter(**filter)
             if not queryset.exists():
                 raise Http404
             return queryset
-        elif title:
-            queryset = self.Meta.model.objects.prefetch_related('channel').filter(title=title)
-            if not queryset.exists():
-                raise Http404
-            return queryset
-        else:
-            return self.Meta.model.objects.prefetch_related('channel').filter(lft=1)
+        return self.queryset
 
     def list(self, request, *args, **kwargs):
-        '''
-        filter categories by title and/or channel description. If 
-        a category is filtered only by title, categories with 
-        this title(if exists) can be sought on all channels. If the same 
-        is filtered only by channel, all categories from this channel(if exists), 
+        """
+        filter categories by title and/or channel description. If
+        a category is filtered only by title, categories with
+        this title(if exists) can be sought on all channels. If the same
+        is filtered only by channel, all categories from this channel(if exists),
         will be sought. If the two query parameters coexist, one category(if exists)
         with her subcategories of a channel will be sought.
-        All data are organized by a json hierarchical structure 
-        '''
+        All data are organized by a json hierarchical structure
+        """
         return super(self.__class__, self).list(request, *args, **kwargs)

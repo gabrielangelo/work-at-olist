182280b4fb8ae90c3998e28874a003fa6212cc4d
gabriel <gabriel@outkeyapp.com>
2017-07-30 17:58:26 -0300

    Script that imports categories refactored

--- src/categories/models.py
+++ src/categories/models.py
@@ -1,6 +1,6 @@
-from django.db import models, transaction
-from django.db.models import F
+from django.db import models
 from .nested_tree import *
+import uuid
 # Create your models here.
 
 
@@ -9,18 +9,21 @@ class Category(models.Model):
     Each node will have two lft and rgt fields which will be the extreme points of their intervals. 
     The lft and rgt fields of their child nodes are within the range of the parent node.
     """
+    id_category = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=50)
-    channel = models.ForeignKey('channels.Channel', related_name='channel')
+    channel = models.ForeignKey('channels.Channel', related_name='channel', on_delete=models.CASCADE)
     rgt = models.IntegerField(default=2)
     lft = models.IntegerField(default=1)
     parent = models.ForeignKey('categories.Category', related_name='parent_node', null=True)
+
+    class Meta:
+        ordering = ['lft', ]
 
     def __repr__(self):
         return '{0} {1}: {2}'.format(self.lft, self.rgt, self.title)
 
     @classmethod
-    def add_node(cls, parent, node, all_nodes):
-        from bulk_update.helper import bulk_update
+    def add_node(cls, nodes):
         """
             Add nodes on the tree. First it checks if the parent node contains child nodes, if 
             it has, the nodes with the fields lft and rgt larger than the rgt field of the parent 
@@ -31,30 +34,27 @@ class Category(models.Model):
             incremented by 2, and the lft and rgt fields are incremented by parent.lft + 1 and 
             parent.rgt + 2.
             """
-        rgt_parent = parent['rgt']
-        lft_parent = parent['lft']
-        nodes = []
+        rgt_parent = nodes[-1].parent.rgt
+        lft_parent = nodes[-1].parent.lft
         last_node = None
-
-        for child in all_nodes.iterator():
-            child.rgt += 2 if child.rgt >= rgt_parent else child.rgt
-            child.lft += 2 if child.lft > rgt_parent else child.lft
-            last_node = child if child.rgt == parent.rgt else last_node
-
-            child.rgt += 2 if child.rgt > lft_parent else child.rgt
-            child.lft += 2 if child.lft > lft_parent else child.lft
-            nodes.append(child)
+        node = nodes[-1]
+        for child in nodes[:-1]:
+            child.rgt = child.rgt + 2 if child.rgt >= rgt_parent else child.rgt
+            child.lft = child.lft + 2 if child.lft > rgt_parent else child.lft
+            last_node = child if child.rgt == rgt_parent - 1 else last_node
 
         if last_node:
             node.lft = last_node.rgt + 1
             node.rgt = last_node.rgt + 2
         else:
-            node.lft = parent.lft + 1
-            node.rgt = parent.lft + 2
+            node.lft = lft_parent + 1
+            node.rgt = lft_parent + 2
 
-        node.parent.id = parent['id']
-        node.save()
-        bulk_update(nodes)
+        return nodes
+
+    @classmethod
+    def persist_nodes(cls, nodes):
+        cls.objects.bulk_create(nodes)
 
     @classmethod
     def make_json_tree(cls, parent):
@@ -82,13 +82,13 @@ Note: - The path works like a stack and its operations are O(1).
         interval = range(parent.lft, parent.rgt+1)
         nodes = cls.objects.filter(channel=parent.channel,
                                    rgt__in=interval, lft__in=interval
-                                   ).order_by('lft')
+                                   ).order_by('lft','title')
 
         path = [] if parent.parent_id is None else [nodes.first()]#the path is used to create the hierarchies in dump_tree method
         t = tree()
         for node in nodes:
 
-            if node.parent_id is None or node.parent_id == path[-1].id:#check if a node is a child of another node that is in path
+            if node.parent_id is None or node.parent_id == path[-1].id_category:#check if a node is a child of another node that is in path
                 path.append(node)
                 dump_tree(t, path)
 
@@ -104,10 +104,9 @@ Note: - The path works like a stack and its operations are O(1).
                 dump_tree(t, path)
 
             elif node.lft > path[-1].rgt + 2:#Check if the level of one node is greater than the other n times
-                while node.parent_id != path[-1].id:
+                while node.parent_id != path[-1].id_category:
                     path.pop()
                 path.append(node)
                 dump_tree(t, path)
 
         return dicts(t)
-
--- src/core/management/commands/importcategories.py
+++ src/core/management/commands/importcategories.py
@@ -1,5 +1,6 @@
 from django.core.management.base import BaseCommand
 from os.path import dirname, abspath
+from django.db import transaction
 from categories.models import Category
 from channels.models import Channel
 from csv import reader
@@ -20,54 +21,72 @@ class Command(BaseCommand):
             default=None
         )
 
+    def print_red(self, name):
+        """red letter"""
+        print("\033[91m {}\033[00m".format(name))
+
+    def print_green(self, name):
+        """grenn letter"""
+        print('\033[92m'.format(name))
+
     def collect_categories(self, channel, file):
-        from django.db import connection
+        """
+        The channel and file parameters match the channel name and file, the file must be in the same directory as
+        the manage.py file
+        """
         format_file = 'csv'
         validate_format = True if format_file in file.split('.') else False
 
         if validate_format:
             base_file = dirname(dirname(dirname(dirname(abspath(__file__))))) + '/' + file
-            query_channel = Channel.objects.filter(description=channel)
-
-            if query_channel.exists():
-                # if a channel exists, delete all you categories, else a new channel is created with your default category
-                Category.objects.prefetch_related('channel').filter(
-                    channel=query_channel.get()).exclude(title='Category').delete()
-                Category.objects.prefetch_related('channel').filter(channel=query_channel.get()).update(lft=1, rgt=2)
-
-            else:
-                Channel.objects.create(description=channel)
+            query_channel, created = Channel.objects.get_or_create(description=channel)
+            if not created:
+                query_channel.channel.filter(channel=query_channel).delete()
 
             with open(base_file, 'r') as f:
                 rows = reader(f)
-                out_nodes = Category.objects.prefetch_related('channel').filter(
-                    channel__description=channel).order_by('lft')
-                root = out_nodes.first()
-                parents = [root]
-                # with transaction.atomic():
-                for row in rows:
-                    if row and '/' in row[0]:
-                        parts = [p for p in row[0].split('/')]
-                        # get the parent node in parts list
-                        check_parents = [i for i in parents[::-1] if i['description'] == parts[:-1][-1].strip()
-                                         and i['parent__description'] == parts[:-1][-2].strip()]
+                root = Category(title='Category', channel=query_channel)
+                nodes = [root]
+                parents = [nodes[0]]
 
-                        node = Category(title=parts[-1:][0].strip(), channel=query_channel.get())
+                with transaction.atomic():
+                    """
+                    The existence of '/' is verified at each iteration in a row. If it does not exist, the root node is 
+                    considered as the parent node of the node to be added, otherwise the previous node is fetched from 
+                    parents and added as the parent node of the node to be added.
+                    
+                    Obs: parents is updated with each Category.add_nodes () call
+                    """
+                    for row in rows:
+                        if row and '/' not in row[0] and row[0] != 'Category':
+                            node = Category(title=row[0].strip(), parent=parents[0], channel=query_channel)
+                            nodes.append(node)
+                            nodes = Category.add_node(nodes=nodes)
+                            self.print_red("Category: %s added" % nodes[-1].title)
+                        elif row and '/' in row[0]:
+                            parts = [p for p in row[0].split('/')]
+                            size_parts = len(parts)
 
-                        parent = (check_parents.pop() if check_parents else
-                                  Category.objects.filter(title=parts[:-1][-1].strip(),
-                                                          channel=query_channel.get(),
-                                                          parent__title=parts[:-1][-2].strip()
-                                                          ).values(
-                                      'id', 'lft', 'rgt', 'description', 'parent__title'))
-                        parents.append(parent)
-                        Category.add_node(parent, node, out_nodes)
+                            if size_parts > 2:
+                                """ 
+                                The loop begins at the end. The probability that a parent node is at the other end of 
+                                the list is larger and makes the search less costly."""
+                                parent = [i for i in nodes[::-1] if i.title == parts[:-1][-1].strip() and
+                                          i.parent.title == parts[:-1][-2].strip()][0]
+                            elif size_parts >= 2:
+                                parent = [i for i in nodes[::-1] if i.title == parts[:-1][-1].strip()
+                                          and i.parent == root][0]
+                            else:
+                                parent = nodes[0]
 
-                    elif row and '/' not in row[0] and row[0] != 'Category':
-                        Category.add_node(root,
-                                          Category(title=row[0], channel=query_channel.get()))
+                            node = Category(title=parts[-1].strip(), parent=parent, channel=query_channel)
+                            nodes.append(node)
+                            nodes = Category.add_node(nodes=nodes)
+                            parents.append(nodes[-1])
 
-                print('%d de teste' % len(connection.queries))
+                            self.print_red("Category: %s added" % nodes[-1].title)
+
+                    Category.persist_nodes(nodes)
         else:
             raise TypeError('Invalid File format')
 

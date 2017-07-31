af487cd97d55d80ebf3e4f57b33a5be2a8120b19
gabrielangelo <=>
2017-07-31 14:48:33 -0300

    changed all migrations to 001_initial.py

--- docs/brief_description.cmd
+++ docs/brief_description.cmd
@@ -1,3 +1,3 @@
 The project was developed in Django version 1.11.3
-nd python version 3.5.2 using the Pycharm IDE and
+and python version 3.5.2 using the Pycharm IDE and
 linux / Ubuntu operating system.
